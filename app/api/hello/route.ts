ac6e1bd04a20605062f5f7b85d2b700f9c684046
wuhao <liuwuhaoo@gmail.com>
2024-03-11 17:28:48 +0100

    fix: ts

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,19 +1,20 @@
 export const dynamic = 'force-dynamic'; // static by default, unless reading the request
 
-import { read } from "fs";
 import {MsEdgeTTS} from "msedge-tts";
 
 // Utility function to collect stream data into a buffer
-function streamToBuffer(stream) {
+function streamToBuffer(stream:any) : Promise<Buffer> {
+  
   return new Promise((resolve, reject) => {
-    const chunks = [];
-    stream.on('data', chunk => chunks.push(chunk));
+    const chunks: Array<any> = [];
+    stream.on('data', (chunk: any) => chunks.push(chunk));
     stream.on('error', reject);
     stream.on('end', () => resolve(Buffer.concat(chunks)));
   });
+
 }
-;
-async function getTTS(num) {
+
+async function getTTS(num: number): Promise<Buffer> {
   const tts = new MsEdgeTTS();
   await tts.setMetadata("de-DE-KatjaNeural", MsEdgeTTS.OUTPUT_FORMATS.WEBM_24KHZ_16BIT_MONO_OPUS);
   const readable = tts.toStream(num.toString());
@@ -26,7 +27,7 @@ export async function GET(request: Request) {
   // console.log(readable)
   // const number = request.query.get('number');
   const num = new URL(request.url).searchParams.get('number');
-  const buffer = await getTTS(num);
+  const buffer = await getTTS(Number(num));
   
   return new Response(buffer, {
     headers: {
@@ -34,8 +35,6 @@ export async function GET(request: Request) {
       'Content-Length': buffer.length.toString(),
     },
   });
-  
-  // return new Response(`Hellooo from ${process.env.VERCEL_REGION}`);
 }
 
 export const runtime = 'nodejs';
